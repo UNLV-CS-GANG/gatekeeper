b4211c5aa1f6d6c93a992c6f763aac94d72ef920
Thien Nguyen <nvktvan@gmail.com>
2023-06-03 23:58:43 -0700

    Set up Eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 //app/page.tsx
-import { UserButton } from "@clerk/nextjs";
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/"/>
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+    plugins: [require.resolve('prettier-plugin-tailwindcss')],
+    semi: false,
+    trailingComma: 'es5',
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: false,
+  }
+  
+module.exports = config
