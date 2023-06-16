745a29b07a20e682637b9da324a461dbf8f1e82b
Emanuel Pecson <emanpecson@gmail.com>
2023-06-16 04:51:49 -0700

    feat: :sparkles: add components for displaying Event data

---
+++ components/EventTable.tsx
@@ -0,0 +1,22 @@
+import { Event } from "@prisma/client";
+import EventRow from "./EventRow";
+
+export default function EventTable({ events }: { events: Event[] }) {
+	return (
+		<div className="container mx-auto">
+			<table className="divide-y divide-gray-700">
+				<thead>
+					<tr>
+						<th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">Title</th>
+						<th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0"> Location</th>
+					</tr>
+				</thead>
+				<tbody>
+					{ events.map((event: Event) => (
+						<EventRow event={event} />
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+}
