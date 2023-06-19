7762f31d459255134cea3b61b936e969856b9c7e
Thien Nguyen <nvktvan@gmail.com>
2023-06-19 03:02:08 -0700

    50% -- merge conflictm, fix this shit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import Navbar from '@/components/Navbar/Navbar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +20,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Navbar />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
