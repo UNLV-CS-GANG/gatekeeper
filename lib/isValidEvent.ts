b8f20068f68d5d2c143aa3a7f4e19fb9f5a800de
Emanuel Pecson <emanpecson@gmail.com>
2023-06-25 10:00:27 -0700

    deprecate unused file

--- lib/isValidEvent.ts
+++
@@ -1,7 +0,0 @@
-import { Event } from "@prisma/client";
-
-export default function isValidEvent(event: Event): boolean {
-	if(!event.title || !event.location || !event.accessStart || !event.accessEnd)
-		return false;
-	return true;
-}
