1272306f574d9fff8752379a94cd5ae5ddae6067
Emanuel Pecson <emanpecson@gmail.com>
2023-06-25 23:30:40 -0700

    refactor to get events by host id

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
-'use client'
-
 import EventTable from '@/components/EventTable'
 import RouteButton from '@/components/RouteButton'
-
-import { useState } from 'react'
-import { Event } from '@prisma/client'
-import useLoadEvent from '@/hooks/useLoadEvent'
+import { auth } from '@clerk/nextjs'
 
 export default function Home() {
-  const [events, setEvents] = useState<Event[]>([])
-	useLoadEvent((data) => { setEvents(data.events) })
+	const { userId } = auth();
 
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="mt-8 mb-4">
-        <RouteButton route="/createEvent">Create Event</RouteButton>
+        <RouteButton route="/createEvent">New Event</RouteButton>
       </div>
 
       <div className="mb-4">
-        <EventTable events={events} />
+        <EventTable userId={userId} />
       </div>
     </div>
   )
