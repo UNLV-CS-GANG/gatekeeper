de57218d73f2ecf546095d26e0f209087719bd9c
Thien Nguyen <nvktvan@gmail.com>
2023-08-10 00:13:26 -0700

    📰

--- app/manageEvent/[id]/page.tsx
+++ app/manageEvent/[id]/page.tsx
@@ -1,36 +1,75 @@
-'use client';
+'use client'
 
-import { useState } from "react";
-import { Event, Invite } from "@prisma/client";
-import RouteButton from "@/components/RouteButton";
-import useLoadData from "@/hooks/useLoadData";
-import { useRouter } from "next/navigation";
+// External Dependencies
+import { useState } from 'react'
+import { utcToZonedTime, format } from 'date-fns-tz'
+import { Event as PrismaEvent, Invite } from '@prisma/client'
+import { useRouter } from 'next/navigation'
+
+// Internal Dependencies
+import RouteButton from '@/components/RouteButton'
+import useLoadData from '@/hooks/useLoadData'
+
+// Extend Prisma Event type
+interface ExtendedEvent extends PrismaEvent {
+  invites?: Invite[]
+}
+
+// Date formatting utility
+function formatToPacific(date?: Date): string {
+  if (!date) return ''
+  const pacificTime = utcToZonedTime(date, 'America/Los_Angeles')
+  return format(pacificTime, 'yyyy-MM-dd', {
+    timeZone: 'America/Los_Angeles',
+  })
+}
+
+// Component for displaying accepted invites
+function AcceptedInvites({ invites }: { invites?: Invite[] }) {
+  // if (!invites?.length) return null
+
+  return (
+    <div className="mb-4">
+      Accepted Invites:
+      {invites?.map((inv, index) => (
+        <li key={index} className="list-none">
+          {inv.firstName} {inv.lastName}
+        </li>
+      ))}
+    </div>
+  )
+}
 
 export default function ManageEvent({ params }: { params: { id: string } }) {
-	const [event, setEvent] = useState<Event>();
-	const router = useRouter();
-	useLoadData((data) => { setEvent(data.event) }, `/api/event?id=${params.id}`);
+  const [event, setEvent] = useState<ExtendedEvent>()
+  const router = useRouter()
 
-	return (
-		<div className="w-full flex h-full ">
-			<div className="w-max m-auto bg-gray-50 rounded-lg ring-1 ring-gray-300 p-4 ">
-				<h1 className="text-lg font-semibold mb-4">{ event?.title }</h1>
-				<p>location: { event?.location }</p>
-				<p>access from { String(event?.accessStart) } to { String(event?.accessEnd) }</p>
-				<p>
-					invite link:
-					<button onClick={() => { router.push(event?.inviteLink) }}>{ event?.inviteLink }</button>
-				</p>
-				
-				<div className="mb-4">
-					Accepted Invites:
-					{ event?.invites.map((inv: Invite, index: number) => (
-						<li key={index} className="list-none">{ inv.firstName } { inv.lastName }</li>
-					)) }
-				</div>
+  useLoadData((data) => {
+    setEvent(data.event)
+  }, `/api/event?id=${params.id}`)
 
-				<RouteButton route='/'>Back</RouteButton>
-			</div>
-		</div>
-	)
+  const handleInviteLinkClick = () => {
+    if (event?.inviteLink) {
+      router.push(event.inviteLink)
+    }
+  }
+
+  return (
+    <div className="flex h-full w-full ">
+      <div className="m-auto w-max rounded-lg bg-gray-50 p-4 ring-1 ring-gray-300 ">
+        <h1 className="mb-4 text-lg font-semibold">{event?.title}</h1>
+        <p>location: {event?.location}</p>
+        <p>
+          access from {formatToPacific(event?.accessStart)} to{' '}
+          {formatToPacific(event?.accessEnd)}
+        </p>
+        <p>
+          invite link:
+          <button onClick={handleInviteLinkClick}>{event?.inviteLink}</button>
+        </p>
+        <AcceptedInvites invites={event?.invites} />
+        <RouteButton route="/">Back</RouteButton>
+      </div>
+    </div>
+  )
 }
