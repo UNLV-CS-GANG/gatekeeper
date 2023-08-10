ad26b6d3af2c91ea2e8e050d0d9793c7ea8c180f
FabianCarrasco <fabian220200@gmail.com>
2023-08-10 00:35:26 -0700

    Did some styling on event manager

--- app/manageEvent/[id]/page.tsx
+++ app/manageEvent/[id]/page.tsx
@@ -9,6 +9,14 @@ import { useRouter } from "next/navigation";
 export default function ManageEvent({ params }: { params: { id: string } }) {
 	const [event, setEvent] = useState<Event>();
 	const router = useRouter();
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric'
+	} as Intl.DateTimeFormatOptions
 	useLoadData((data) => { setEvent(data.event) }, `/api/event?id=${params.id}`);
 
 	return (
@@ -16,10 +24,10 @@ export default function ManageEvent({ params }: { params: { id: string } }) {
 			<div className="w-max m-auto bg-gray-50 rounded-lg ring-1 ring-gray-300 p-4 ">
 				<h1 className="text-lg font-semibold mb-4">{ event?.title }</h1>
 				<p>location: { event?.location }</p>
-				<p>access from { String(event?.accessStart) } to { String(event?.accessEnd) }</p>
+				<p>access from { String(new Date(event?.accessStart as Date).toLocaleDateString('en-US', options)) } to { String(new Date(event?.accessEnd as Date).toLocaleDateString('en-US', options)) }</p>
 				<p>
 					invite link:
-					<button onClick={() => { router.push(event?.inviteLink) }}>{ event?.inviteLink }</button>
+					<button onClick={() => { router.push((event as Event)?.inviteLink) }}>{ (event as Event)?.inviteLink }</button>
 				</p>
 				
 				<div className="mb-4">
