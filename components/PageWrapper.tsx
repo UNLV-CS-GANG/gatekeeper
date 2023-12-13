ddb8a1e5754124641f0ef57e8be1826546bcda1b
Emanuel Pecson <emanpecson@gmail.com>
2023-12-12 18:22:04 -0800

    style: :lipstick: remove left padding

---
+++ components/PageWrapper.tsx
@@ -0,0 +1,28 @@
+export default function PageWrapper({
+  title,
+  description,
+  children,
+}: {
+  title: string
+  description: string
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <div className="py-8">
+        {/* title */}
+        <div>
+          <h1 className="text-3xl font-medium text-gray-600">{title}</h1>
+        </div>
+
+        {/* description */}
+        <div>
+          <p className="w-1/2 font-normal text-gray-400">{description}</p>
+        </div>
+      </div>
+
+      {/* content */}
+      <div>{children}</div>
+    </>
+  )
+}
