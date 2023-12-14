28952aa32b2f05afecd3995dbdc9efed645ba3cc
Emanuel Pecson <emanpecson@gmail.com>
2023-12-13 21:08:31 -0800

    style: :lipstick: update table height and header placement

--- components/Event/EventTable.tsx
+++ components/Event/EventTable.tsx
@@ -18,30 +18,32 @@ export default function EventTable({ userId }: { userId: string | null }) {
 
   return (
     <>
-      <div className="max-h-80 overflow-auto rounded-xl bg-white px-4 py-4">
-        <table className="w-full">
-          <thead>
-            <tr className="bg-white">
-              <th className="sticky top-0 bg-white py-4 pl-3 text-left text-sm font-semibold uppercase text-gray-400">
-                Title
-              </th>
-              <th className="sticky top-0 bg-white py-4 text-center text-sm font-semibold uppercase text-gray-400">
-                Location
-              </th>
-              <th className="sticky top-0 bg-white py-4 text-center text-sm font-semibold uppercase text-gray-400">
-                Access ends
-              </th>
-              <th className="sticky top-0 bg-white py-4 pr-3 text-right text-sm font-semibold uppercase text-gray-400">
-                Created
-              </th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200 bg-white">
-            {events.map((event: Event, index: number) => (
-              <EventRow key={index} event={event as EventExtended} />
-            ))}
-          </tbody>
-        </table>
+      <div className="rounded-xl bg-white py-4">
+        <div className="max-h-[30rem] overflow-auto px-4">
+          <table className="w-full">
+            <thead>
+              <tr className="bg-white">
+                <th className="sticky top-0 bg-white py-4 pl-3 text-left text-sm font-semibold uppercase text-gray-400">
+                  Title
+                </th>
+                <th className="sticky top-0 bg-white py-4 text-center text-sm font-semibold uppercase text-gray-400">
+                  Location
+                </th>
+                <th className="sticky top-0 bg-white py-4 text-center text-sm font-semibold uppercase text-gray-400">
+                  Access ends
+                </th>
+                <th className="sticky top-0 bg-white py-4 pr-3 text-right text-sm font-semibold uppercase text-gray-400">
+                  Created
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200 bg-white">
+              {events.map((event: Event, index: number) => (
+                <EventRow key={index} event={event as EventExtended} />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   )
