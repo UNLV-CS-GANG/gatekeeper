2f9f67c6be4375c4d5e75870b8259a4587c5ef11
Emanuel Pecson <emanpecson@gmail.com>
2023-12-13 20:59:43 -0800

    refactor: :recycle: provide userId in fetch

--- lib/authFetch.ts
+++ lib/authFetch.ts
@@ -1,24 +1,24 @@
 'use server'
 
-import { auth }  from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs'
 
 type FetchOptions = {
-	method: string;
-	body: string;
+  method: string
+  body: string
 }
 
 export default async function authFetch(url: string, options: FetchOptions) {
-	const { getToken } = auth();
-	const accessToken = await getToken();
+  const { getToken, userId } = auth()
+  const accessToken = await getToken()
 
-	const res = await fetch(url, {
-		method: options.method,
-		headers: {
-			'Content-Type': 'application/json',
-				Authorization: `Bearer ${accessToken}`,
-		},
-		body: options.body,
-	});
+  const res = await fetch(url, {
+    method: options.method,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ content: options.body, userId }),
+  })
 
-	return res;
+  return res
 }
