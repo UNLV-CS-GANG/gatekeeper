0af6e46f17d1de1c3dbdbf555ab10bde294eda53
Emanuel Pecson <emanpecson@gmail.com>
2023-12-17 13:46:49 -0800

    fix: :bug: comment out bug

--- app/manageEvent/[id]/page.tsx
+++ app/manageEvent/[id]/page.tsx
@@ -1,36 +1,40 @@
-'use client';
+'use client'
 
-import { useState } from "react";
-import { Event, Invite } from "@prisma/client";
-import RouteButton from "@/components/RouteButton";
-import useLoadData from "@/hooks/useLoadData";
-import { useRouter } from "next/navigation";
+import { useState } from 'react'
+import { Event, Invite } from '@prisma/client'
+import RouteButton from '@/components/RouteButton'
+import useLoadData from '@/hooks/useLoadData'
+import { useRouter } from 'next/navigation'
 
 export default function ManageEvent({ params }: { params: { id: string } }) {
-	const [event, setEvent] = useState<Event>();
-	const router = useRouter();
-	useLoadData((data) => { setEvent(data.event) }, `/api/event?id=${params.id}`);
+  const [event, setEvent] = useState<Event>()
+  const router = useRouter()
+  useLoadData((data) => {
+    setEvent(data.event)
+  }, `/api/event?id=${params.id}`)
 
-	return (
-		<div>
-			<p>temp page layout for now...</p>
-			<p>id: { event?.id }</p>
-			<p>title: { event?.title }</p>
-			<p>location: { event?.location }</p>
-			<p>access from { String(event?.accessStart) } to { String(event?.accessEnd) }</p>
-			<p>
-				invite link:
-				<button onClick={() => { router.push(event?.inviteLink) }}>{ event?.inviteLink }</button>
-			</p>
-			
-			<div>
+  return (
+    <div>
+      <p>temp page layout for now...</p>
+      <p>id: {event?.id}</p>
+      <p>title: {event?.title}</p>
+      <p>location: {event?.location}</p>
+      <p>
+        access from {String(event?.accessStart)} to {String(event?.accessEnd)}
+      </p>
+      <p>
+        invite link:
+        {/* <button onClick={() => { router.push(event?.inviteLink) }}>{ event?.inviteLink }</button> */}
+      </p>
+
+      {/* <div>
 				Accepted Invites:
 				{ event?.invites.map((inv: Invite, index: number) => (
 					<li key={index}>{ inv.firstName } { inv.lastName }</li>
 				)) }
-			</div>
+			</div> */}
 
-			<RouteButton route='/'>Back</RouteButton>
-		</div>
-	)
+      <RouteButton route="/">Back</RouteButton>
+    </div>
+  )
 }
