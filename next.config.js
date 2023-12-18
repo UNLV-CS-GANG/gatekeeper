ab2fdc09c178e3ec0661170b1226fd4c7c9415b4
Emanuel Pecson <emanpecson@gmail.com>
2023-12-18 14:57:38 -0800

    feat: :sparkles: send email with qr code on accepted invite

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverActions: true
-	},
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: [
+      '@react-email/components',
+      '@react-email/render',
+      '@react-email/tailwind',
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ types/email/EmailQrProps.ts
@@ -0,0 +1,11 @@
+type EmailQrProps = {
+  qrSrc: string
+  title: string
+  description?: string
+  location: string
+  accessStart: string
+  accessEnd: string
+  username: string
+}
+
+export default EmailQrProps
