ce9209e308908e3f6bb55ac2b4e5cb33b0b92004
Emanuel Pecson <emanpecson@gmail.com>
2023-12-19 16:05:28 -0800

    refactor: :recycle: route user to dashboard on sign-up

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs'
 export default function Signup() {
   return (
     <div className="flex h-screen w-screen place-items-center justify-center bg-gray-100">
-      <SignUp />
+      <SignUp afterSignUpUrl={'/myEvents'} />
     </div>
   )
 }
