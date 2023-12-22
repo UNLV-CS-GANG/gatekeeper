dc0643f8f3f110924686ee2ecc989c1a14e00ddc
Emanuel Pecson <emanpecson@gmail.com>
2023-12-22 12:07:14 -0800

    refactor: :recycle: add width prop to modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -6,7 +6,7 @@ import classNames from '@/lib/classNames'
 export default function Modal({
   isOpen,
   onClose,
-  width = 'max-w-3xl',
+  width = 'sm:max-w-3xl max-w-xs',
   children,
 }: {
   isOpen: boolean
@@ -37,7 +37,7 @@ export default function Modal({
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex place-items-center items-end justify-center text-center sm:min-h-full sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -49,11 +49,11 @@ export default function Modal({
             >
               <Dialog.Panel
                 className={classNames(
-                  'relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full',
+                  'relative my-8 w-full transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all',
                   width
                 )}
               >
-                <div className="absolute right-5 top-5 z-20">
+                <div className="absolute right-2 top-2 z-20 sm:right-5 sm:top-5">
                   <button
                     type="button"
                     className="rounded-full p-1 outline-none transition-colors duration-150 hover:bg-gray-200"
