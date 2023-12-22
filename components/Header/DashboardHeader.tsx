442841d32edd4d5120322851a4059255cec83c09
Emanuel Pecson <emanpecson@gmail.com>
2023-12-22 12:07:04 -0800

    refactor: :recycle: allow route back to landing page from header-title button

--- components/Header/DashboardHeader.tsx
+++ components/Header/DashboardHeader.tsx
@@ -3,12 +3,17 @@
 import ToggleTheme from './ToggleTheme'
 import NotificationBell from './NotificationBell'
 import { UserButton } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
 
 export default function DashboardHeader() {
+  const router = useRouter()
+
   return (
     <div className="grid h-16 w-full grid-cols-12 justify-between bg-white shadow-sm">
       <div className="col-span-3 flex place-items-center justify-center">
-        <h1 className="text-2xl font-medium">gatekeeper</h1>
+        <h1 className="text-2xl font-medium">
+          <button onClick={() => router.push('/')}>gatekeeper</button>
+        </h1>
       </div>
 
       <div className="col-span-9 flex place-items-center justify-end space-x-5 pr-10">
