26034e821d634bf534f0c631a2a5762a7eb70da7
Emanuel Pecson <emanpecson@gmail.com>
2023-12-23 02:30:22 -0800

    style: :lipstick: update event info modal view

--- components/Event/EventModal/InfoView.tsx
+++ components/Event/EventModal/InfoView.tsx
@@ -24,41 +24,45 @@ export default function InfoView({
 
   return (
     <>
-      <div className="px-7 py-6">
+      <div className="p-4 sm:px-7 sm:py-6">
         <div className="pb-4">
-          <h1 className="text-2xl font-medium">{event.title}</h1>
-          <p className="text-gray-500">{event.description}</p>
+          <h1 className="text-xl font-medium sm:text-2xl">{event.title}</h1>
+          <p className="text-sm text-gray-500 sm:text-base">
+            {event.description}
+          </p>
         </div>
-        <ul className="divide-y py-10">
-          <li className="flex py-1.5">
-            <p className="w-1/5 text-sm font-semibold uppercase text-gray-500">
+        <ul className="divide-y py-5 sm:py-10">
+          <li className="py-1.5 sm:flex">
+            <p className="pb-1.5 text-xs font-semibold uppercase text-gray-500 sm:w-1/5 sm:pb-0 sm:text-sm">
               Location
             </p>
-            <p className="w-4/5 text-gray-800">{event.location}</p>
+            <p className="text-sm text-gray-800 sm:w-4/5 sm:text-base">
+              {event.location}
+            </p>
           </li>
-          <li className="flex py-1.5">
-            <p className="w-1/5 text-sm font-semibold uppercase text-gray-500">
+          <li className="py-1.5 sm:flex">
+            <p className="pb-1.5 text-xs font-semibold uppercase text-gray-500 sm:w-1/5 sm:pb-0 sm:text-sm">
               Created
             </p>
-            <p className="w-4/5 text-gray-800">
+            <p className="text-sm text-gray-800 sm:w-4/5 sm:text-base">
               {getDateTime(new Date(event.createdAt))}
             </p>
           </li>
-          <li className="flex py-1.5">
-            <p className="w-1/5 text-sm font-semibold uppercase text-gray-500">
+          <li className="py-1.5 sm:flex">
+            <p className="pb-1.5 text-xs font-semibold uppercase text-gray-500 sm:w-1/5 sm:pb-0 sm:text-sm">
               Access
             </p>
-            <p className="w-4/5 text-gray-800">
+            <p className="text-sm text-gray-800 sm:w-4/5 sm:text-base">
               {getDateTime(new Date(event.accessStart))} {' - '}
               {getDateTime(new Date(event.accessEnd))}
             </p>
           </li>
-          <li className="flex py-1.5">
-            <p className="w-1/5 text-sm font-semibold uppercase text-gray-500">
+          <li className="py-1.5 sm:flex">
+            <p className="pb-1.5 text-xs font-semibold uppercase text-gray-500 sm:w-1/5 sm:pb-0 sm:text-sm">
               Verifier code
             </p>
 
-            <div className="flex w-4/5 space-x-2.5">
+            <div className="flex space-x-2.5 text-sm sm:w-4/5 sm:text-base">
               <button
                 className="rounded-full bg-gray-200 p-0.5 transition-colors duration-150 hover:bg-gray-300"
                 onClick={() => setVerifierCodeIsVisible(!verifierCodeIsVisible)}
@@ -79,11 +83,11 @@ export default function InfoView({
               </div>
             </div>
           </li>
-          <li className="flex py-1.5">
-            <p className="w-1/5 text-sm font-semibold uppercase text-gray-500">
+          <li className="py-1.5 sm:flex">
+            <p className="pb-1.5 text-xs font-semibold uppercase text-gray-500 sm:w-1/5 sm:pb-0 sm:text-sm">
               Invite link
             </p>
-            <div className="w-4/5 text-blue-600 transition-colors duration-200 hover:text-blue-400">
+            <div className="flex break-all text-sm text-blue-600 transition-colors duration-200 hover:text-blue-400 sm:w-4/5 sm:text-base">
               <button
                 onClick={() => {
                   router.push(event?.inviteLink)
@@ -111,17 +115,17 @@ export default function InfoView({
           </div>
         </div>
         {displayInvites.length > 0 && (
-          <div className="max-h-48 overflow-y-auto rounded-lg bg-gray-100 px-4 py-2">
+          <div className="max-h-48 overflow-y-auto rounded-lg bg-gray-100 px-1.5 py-2 sm:px-4">
             <table className="w-full">
               <thead>
                 <tr>
-                  <th className="sticky top-0 rounded-l-lg bg-gray-300 bg-opacity-60 pl-3 text-sm font-semibold uppercase text-gray-600 backdrop-blur-lg">
+                  <th className="sticky top-0 rounded-l-lg bg-gray-300 bg-opacity-60 pl-1.5 text-xs font-semibold uppercase text-gray-600 backdrop-blur-lg sm:pl-3 sm:text-sm">
                     Name
                   </th>
-                  <th className="sticky top-0 bg-gray-300 bg-opacity-60 text-sm font-semibold uppercase text-gray-600 backdrop-blur-lg">
+                  <th className="sticky top-0 hidden bg-gray-300 bg-opacity-60 text-xs font-semibold uppercase text-gray-600 backdrop-blur-lg sm:block sm:text-sm">
                     Email
                   </th>
-                  <th className="sticky top-0 rounded-r-lg bg-gray-300 bg-opacity-60 pr-3 text-right text-sm font-semibold uppercase text-gray-600 backdrop-blur-lg">
+                  <th className="sticky top-0 rounded-r-lg bg-gray-300 bg-opacity-60 pr-1.5 text-right text-xs font-semibold uppercase text-gray-600 backdrop-blur-lg sm:pr-3 sm:text-sm">
                     Accepted
                   </th>
                 </tr>
@@ -133,13 +137,13 @@ export default function InfoView({
                     onClick={() => onClickInvite(inv, index)}
                     className="cursor-pointer transition-colors duration-150 hover:bg-gray-200"
                   >
-                    <td className="whitespace-normal rounded-l-lg py-1.5 pl-3 text-gray-800">
+                    <td className="whitespace-normal rounded-l-lg py-2.5 pl-1.5 text-sm text-gray-800 sm:py-1.5 sm:pl-3 sm:text-base">
                       {inv.firstName} {inv.lastName}
                     </td>
-                    <td className="whitespace-nowrap py-1.5 text-gray-800">
+                    <td className="hidden whitespace-normal py-2.5 text-sm text-gray-800 sm:block sm:whitespace-nowrap sm:py-1.5 sm:text-base">
                       {inv.email}
                     </td>
-                    <td className="whitespace-nowrap rounded-r-lg py-1.5 pr-3 text-right text-gray-800">
+                    <td className="whitespace-normal rounded-r-lg py-2.5 pr-1.5 text-right text-sm text-gray-800 sm:whitespace-nowrap sm:py-1.5 sm:pr-3 sm:text-base">
                       {getDateTime(new Date(inv.acceptedAt))}
                     </td>
                   </tr>
