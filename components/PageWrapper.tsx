8e586b8cc8c24721adda9c8bdb2a2d0740dffd4a
Emanuel Pecson <emanpecson@gmail.com>
2023-12-25 21:56:16 -0800

    style: :lipstick: tighter page wrapper on mobile

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -9,17 +9,17 @@ export default function PageWrapper({
 }) {
   return (
     <div className="h-screen">
-      <div className="pb-8">
+      <div className="pb-5 sm:pb-8">
         {/* title */}
         <div>
-          <h1 className="text-2xl font-medium text-gray-600 sm:text-3xl">
+          <h1 className="text-lg font-medium text-gray-600 sm:text-3xl">
             {title}
           </h1>
         </div>
 
         {/* description */}
         <div>
-          <p className="w-2/3 text-sm font-normal text-gray-400 sm:w-1/2 sm:text-base">
+          <p className="w-2/3 text-xs font-normal text-gray-400 sm:w-1/2 sm:text-base">
             {description}
           </p>
         </div>
