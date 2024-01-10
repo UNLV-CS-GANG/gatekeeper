32d0c8f5fca0196b0c1c6f42c9f31ba495cb36b0
Emanuel Pecson <emanpecson@gmail.com>
2024-01-09 18:14:43 -0800

    style: :lipstick: setup new landing page

--- app/(guest)/layout.tsx
+++ app/(guest)/layout.tsx
@@ -6,11 +6,11 @@ export default function GuestLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="relative h-screen w-screen overflow-hidden">
+    <div className="relative h-screen w-screen">
       <div className="fixed top-0 w-full">
         <GuestHeader />
       </div>
-      <div className="h-full bg-gray-100 px-10 pt-24">{children}</div>
+      <div className="h-full px-10 pt-24">{children}</div>
     </div>
   )
 }
