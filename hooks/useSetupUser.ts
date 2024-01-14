aac510ad234933d1fbf84077591775d405490ee8
Emanuel Pecson <emanpecson@gmail.com>
2024-01-13 22:18:35 -0800

    refactor: :recycle: setup User at dashboard

--- hooks/useSetupUser.ts
+++ hooks/useSetupUser.ts
@@ -4,35 +4,39 @@
 	Create User in database if it doesn't exist
 */
 
-import { clerkClient, useAuth } from '@clerk/nextjs'
+import { useAuth } from '@clerk/nextjs'
 import { User } from '@prisma/client'
+import { useEffect } from 'react'
 
 export default async function useSetupUser() {
   const { userId } = useAuth()
-  const clerkUser = await clerkClient.users.getUser(String(userId))
 
-  try {
-    const res = await fetch(`/api/user?id=${userId}`, {
-      method: 'GET',
-    })
+  useEffect(() => {
+    const setupUser = async () => {
+      try {
+        const res = await fetch(`/api/user?id=${userId}`, {
+          method: 'GET',
+        })
 
-    const data = await res.json()
+        const data = await res.json()
 
-    // if not found, link user in db
-    if (!data) {
-      const res = await fetch(`/api/user`, {
-        method: 'POST',
-        body: JSON.stringify({
-          id: userId,
-          firstName: clerkUser.firstName,
-          lastName: clerkUser.lastName,
-          username: clerkUser.username,
-        } as User),
-      })
+        // if not found, link user in db
+        if (!data) {
+          const res = await fetch(`/api/user`, {
+            method: 'POST',
+            body: JSON.stringify({
+              id: userId,
+            } as User),
+          })
 
-      console.log('created user:', await res.json())
+          console.log('created user:', await res.json())
+        } else {
+          console.log('user exists')
+        }
+      } catch (error) {
+        console.error(error)
+      }
     }
-  } catch (error) {
-    console.error(error)
-  }
+    setupUser()
+  }, [])
 }
