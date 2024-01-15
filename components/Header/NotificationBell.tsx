3f0b3ccfd9cc08f17b9784e2f802783fe65d6442
Thien Nguyen <nvktvan@gmail.com>
2024-01-15 03:45:39 -0800

    💮 bugs: notification bell, GONE?

--- components/Header/NotificationBell.tsx
+++ components/Header/NotificationBell.tsx
@@ -74,63 +74,61 @@ export default function NotificationBell() {
   }
 
   return (
-    <>
-      <Popover className="relative">
-        <Popover.Button className="relative flex outline-none" onClick={() => setIsOpen(!isOpen)}>
-          {notifications && notifications.length > 0 && (
-            <div className="absolute right-0 top-0 flex h-[1.15rem] w-[1.15rem] place-items-center justify-center rounded-full bg-orange-600 text-[0.7rem] text-gray-200">
-              {notifications.length}
-            </div>
-          )}
-          <HeaderIcon Icon={BellIcon} />
-        </Popover.Button>
+    <Popover className="relative">
+      <Popover.Button className="relative flex outline-none" onClick={() => setIsOpen(!isOpen)}>
+        {notifications && notifications.length > 0 && (
+          <div className="absolute right-0 top-0 flex h-[1.15rem] w-[1.15rem] place-items-center justify-center rounded-full bg-orange-600 text-[0.7rem] text-gray-200">
+            {notifications.length}
+          </div>
+        )}
+        <HeaderIcon Icon={BellIcon} />
+      </Popover.Button>
 
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-200"
-          enterFrom="opacity-0 translate-y-1"
-          enterTo="opacity-100 translate-y-0"
-          leave="transition ease-in duration-150"
-          leaveFrom="opacity-100 translate-y-0"
-          leaveTo="opacity-0 translate-y-1"
-        >
-          <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
-            <div className="w-[16rem] max-w-xs flex-auto overflow-hidden rounded-2xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 sm:w-[20rem]">
-              <div className="p-2">
-                {notifications && notifications.length !== 0 ? (
-                  <div className="space-y-2">
-                    <div className="max-h-[18.5rem] overflow-y-auto">
-                      {notifications.map((noti: Notification, index: number) => (
-                        <div
-                          key={index}
-                          className="relative space-y-1.5 rounded-lg p-4 text-sm transition-colors duration-100 hover:bg-gray-50"
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-1"
+      >
+        <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+          <div className="w-[16rem] max-w-xs flex-auto overflow-hidden rounded-2xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 sm:w-[20rem]">
+            <div className="p-2">
+              {notifications && notifications.length !== 0 ? (
+                <div className="space-y-2">
+                  <div className="max-h-[18.5rem] overflow-y-auto">
+                    {notifications.map((noti: Notification, index: number) => (
+                      <div
+                        key={index}
+                        className="relative space-y-1.5 rounded-lg p-4 text-sm transition-colors duration-100 hover:bg-gray-50"
+                      >
+                        <p className="mr-5 text-gray-800">{noti.content}</p>
+                        <p className="text-gray-400">{getDateTime(new Date(noti.notifiedAt))}</p>
+                        <button
+                          className="absolute right-2 top-2 rounded-full p-0.5 text-gray-400 transition-colors duration-100 hover:bg-gray-200 hover:text-gray-600"
+                          onClick={() => clearNotification(noti.id, index)}
                         >
-                          <p className="mr-5 text-gray-800">{noti.content}</p>
-                          <p className="text-gray-400">{getDateTime(new Date(noti.notifiedAt))}</p>
-                          <button
-                            className="absolute right-2 top-2 rounded-full p-0.5 text-gray-400 transition-colors duration-100 hover:bg-gray-200 hover:text-gray-600"
-                            onClick={() => clearNotification(noti.id, index)}
-                          >
-                            <XMarkIcon className="h-5 w-5" />
-                          </button>
-                        </div>
-                      ))}
-                    </div>
-                    <button
-                      className="flex w-full items-center justify-center gap-x-2.5 rounded-lg bg-gray-100 p-2 text-gray-500 transition-colors duration-150 hover:bg-gray-200"
-                      onClick={clearAllNotifications}
-                    >
-                      Clear all
-                    </button>
+                          <XMarkIcon className="h-5 w-5" />
+                        </button>
+                      </div>
+                    ))}
                   </div>
-                ) : (
-                  <div className="flex justify-center text-gray-500">No notifications</div>
-                )}
-              </div>
+                  <button
+                    className="flex w-full items-center justify-center gap-x-2.5 rounded-lg bg-gray-100 p-2 text-gray-500 transition-colors duration-150 hover:bg-gray-200"
+                    onClick={clearAllNotifications}
+                  >
+                    Clear all
+                  </button>
+                </div>
+              ) : (
+                <div className="flex justify-center text-gray-500">No notifications</div>
+              )}
             </div>
-          </Popover.Panel>
-        </Transition>
-      </Popover>
-    </>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   )
 }
