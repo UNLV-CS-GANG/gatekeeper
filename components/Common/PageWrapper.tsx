fb3908d7cb6e981be6eb842c877f983db7bcbab6
Emanuel Pecson <emanpecson@gmail.com>
2024-04-24 12:53:13 -0700

    style: :lipstick: remove page header on mobile

--- components/Common/PageWrapper.tsx
+++ components/Common/PageWrapper.tsx
@@ -9,19 +9,12 @@ export default function PageWrapper({
 }) {
   return (
     <div className="h-screen">
-      <div className="pb-5 sm:pb-8">
-        {/* title */}
-        <div>
-          <h1 className="text-lg font-medium text-gray-600 sm:text-3xl">{title}</h1>
-        </div>
-
-        {/* description */}
-        <div>
-          <p className="w-2/3 text-xs font-normal text-gray-400 sm:w-1/2 sm:text-base">{description}</p>
-        </div>
+      <div className="hidden pb-8 sm:block">
+        <h1 className="text-lg font-medium text-gray-600 sm:text-3xl">{title}</h1>
+        <p className="w-2/3 text-xs font-normal text-gray-400 sm:w-1/2 sm:text-base">{description}</p>
       </div>
 
-      {/* content */}
+      {/* page content */}
       <div>{children}</div>
     </div>
   )
