84ca5431dd9d027029ff23f6cdcf2bb17da71d8c
Emanuel Pecson <emanpecson@gmail.com>
2024-04-26 21:49:59 -0700

    refactor: :recycle: specify generateCode return type as string

--- lib/generateCode.ts
+++ lib/generateCode.ts
@@ -3,5 +3,5 @@ import { nanoid } from 'nanoid'
 export default function generateCode() {
   const code = nanoid(12)
   const formattedCode = code.match(/.{1,4}/g)?.join('-')
-  return formattedCode
+  return formattedCode as string
 }
