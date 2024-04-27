05e8403daf7c152a09e84a56a5405b4154d952f7
Emanuel Pecson <emanpecson@gmail.com>
2024-04-27 01:29:12 -0700

    refactor: :recycle: modularize table skip button as own component

--- app/(dashboard)/(eventHosting)/manageEvents/page.tsx
+++ app/(dashboard)/(eventHosting)/manageEvents/page.tsx
@@ -4,11 +4,10 @@ import EventTable from '@/components/Event/EventTable'
 import EventTableTabs from '@/components/Event/EventTableTabs'
 import SearchBar from '@/components/Event/SearchBar'
 import PageWrapper from '@/components/Common/PageWrapper'
-import classNames from '@/lib/classNames'
 import { useAuth } from '@clerk/nextjs'
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
 import { Event } from '@prisma/client'
 import { useEffect, useState } from 'react'
+import Iterator from '@/components/Common/Iterator'
 
 export default function ManageEvents() {
   interface EventsResponse {
@@ -87,44 +86,13 @@ export default function ManageEvents() {
         isHost={true}
       />
 
-      {/* table skip button */}
-      <div className="flex justify-center sm:justify-end">
-        <div className="relative flex w-full space-x-4 rounded-full bg-gray-200 px-4 py-2.5 sm:w-56 sm:justify-between">
-          <div className="flex place-items-center justify-center space-x-2">
-            <button
-              className="absolute left-3 cursor-pointer rounded-full p-1 transition-colors duration-150 hover:bg-gray-300 disabled:cursor-default disabled:hover:bg-gray-200 sm:static"
-              onClick={() => setTableSkips(tableSkips - 1)}
-              disabled={tableSkips === 0}
-            >
-              <ArrowLeftIcon
-                className={classNames('h-5 w-5', tableSkips === 0 ? 'text-gray-400 text-opacity-50' : 'text-gray-600')}
-              />
-            </button>
-            <button
-              className="absolute right-3 rounded-full p-1 transition-colors duration-150 hover:bg-gray-300 disabled:cursor-default disabled:hover:bg-gray-200 sm:static"
-              onClick={() => setTableSkips(tableSkips + 1)}
-              disabled={(tableSkips + 1) * rows >= allEventsCount}
-            >
-              <ArrowRightIcon
-                className={classNames(
-                  'h-5 w-5',
-                  (tableSkips + 1) * rows >= allEventsCount ? 'text-gray-400 text-opacity-50' : 'text-gray-600'
-                )}
-              />
-            </button>
-          </div>
-          <div className="flex w-full justify-center space-x-2 sm:justify-end">
-            <p className="font-medium text-gray-600">
-              {events.length > 0
-                ? `${tableSkips * rows + 1} - ${
-                    tableSkips * rows + rows <= allEventsCount ? tableSkips * rows + rows : allEventsCount
-                  }`
-                : '0 - 0'}
-            </p>
-            <p className="font-medium text-gray-600">{`/ ${allEventsCount}`}</p>
-          </div>
-        </div>
-      </div>
+      <Iterator
+        itemsCount={events.length}
+        allItemsCount={allEventsCount}
+        itemsToDisplay={rows}
+        skips={tableSkips}
+        setSkips={setTableSkips}
+      />
     </PageWrapper>
   )
 }
---
+++ components/Common/Iterator.tsx
@@ -0,0 +1,59 @@
+import classNames from '@/lib/classNames'
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
+import { Dispatch, SetStateAction } from 'react'
+
+export default function Iterator({
+  itemsCount,
+  allItemsCount,
+  itemsToDisplay,
+  skips,
+  setSkips,
+}: {
+  itemsCount: number
+  allItemsCount: number
+  itemsToDisplay: number
+  skips: number
+  setSkips: Dispatch<SetStateAction<number>>
+}) {
+  return (
+    <div className="flex justify-center sm:justify-end">
+      <div className="relative flex w-full space-x-4 rounded-full bg-gray-200 px-4 py-2.5 sm:w-56 sm:justify-between">
+        <div className="flex place-items-center justify-center space-x-2">
+          <button
+            className="absolute left-3 cursor-pointer rounded-full p-1 transition-colors duration-150 hover:bg-gray-300 disabled:cursor-default disabled:hover:bg-gray-200 sm:static"
+            onClick={() => setSkips(skips - 1)}
+            disabled={skips === 0}
+          >
+            <ArrowLeftIcon
+              className={classNames('h-5 w-5', skips === 0 ? 'text-gray-400 text-opacity-50' : 'text-gray-600')}
+            />
+          </button>
+          <button
+            className="absolute right-3 rounded-full p-1 transition-colors duration-150 hover:bg-gray-300 disabled:cursor-default disabled:hover:bg-gray-200 sm:static"
+            onClick={() => setSkips(skips + 1)}
+            disabled={(skips + 1) * itemsToDisplay >= allItemsCount}
+          >
+            <ArrowRightIcon
+              className={classNames(
+                'h-5 w-5',
+                (skips + 1) * itemsToDisplay >= allItemsCount ? 'text-gray-400 text-opacity-50' : 'text-gray-600'
+              )}
+            />
+          </button>
+        </div>
+        <div className="flex w-full justify-center space-x-2 sm:justify-end">
+          <p className="font-medium text-gray-600">
+            {itemsCount > 0
+              ? `${skips * itemsToDisplay + 1} - ${
+                  skips * itemsToDisplay + itemsToDisplay <= allItemsCount
+                    ? skips * itemsToDisplay + itemsToDisplay
+                    : allItemsCount
+                }`
+              : '0 - 0'}
+          </p>
+          <p className="font-medium text-gray-600">{`/ ${allItemsCount}`}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
