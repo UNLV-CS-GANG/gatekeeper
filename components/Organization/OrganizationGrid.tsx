1fad86313349986935df34b94bcd966c2522c238
Emanuel Pecson <emanpecson@gmail.com>
2024-04-27 19:28:47 -0700

    feat: :sparkles: show state for no-data in organizations grid

--- components/Organization/OrganizationGrid.tsx
+++ components/Organization/OrganizationGrid.tsx
@@ -1,6 +1,7 @@
 import OrganizationListItem from './OrganizationListItem'
 import { OrganizationExtended } from '@/types/Organization/OrganizationExtended'
 import OrganizationListItemLoading from './OrganizationListItemLoading'
+import NoData from '../State/NoData'
 
 export default function OrganizationGrid({
   organizations,
@@ -13,14 +14,20 @@ export default function OrganizationGrid({
 }) {
   return (
     <ul className="sm:gap-5 lg:grid lg:grid-cols-2 xl:grid-cols-3">
+      {isLoadingOrganizations &&
+        new Array(itemsToDisplay).fill(0).map((_: number, i: number) => <OrganizationListItemLoading key={i} />)}
+
       {!isLoadingOrganizations &&
         organizations.length > 0 &&
         organizations.map((org: OrganizationExtended, i: number) => (
           <OrganizationListItem key={i} organization={org} />
         ))}
 
-      {isLoadingOrganizations &&
-        new Array(itemsToDisplay).fill(0).map((_: number, i: number) => <OrganizationListItemLoading key={i} />)}
+      {!isLoadingOrganizations && organizations.length === 0 && (
+        <li className="flex h-48 place-items-center justify-center bg-white drop-shadow-md">
+          <NoData />
+        </li>
+      )}
     </ul>
   )
 }
