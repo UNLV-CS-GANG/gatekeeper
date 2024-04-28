8f1373d25a31ade1687486f4c31cda9bcc9c5f91
Emanuel Pecson <emanpecson@gmail.com>
2024-04-27 22:54:17 -0700

    refactor: :recycle: add functionality to create-button that routes to new page

--- components/Event/Preview/EventGrid.tsx
+++ components/Event/Preview/EventGrid.tsx
@@ -3,6 +3,7 @@ import EventExtended from '@/types/EventExtended'
 import EventGridItem from './EventGridItem'
 import EventGridItemLoading from './EventGridItemLoading'
 import { PlusCircleIcon } from '@heroicons/react/24/outline'
+import { useRouter } from 'next/navigation'
 
 export default function EventGrid({
   events,
@@ -15,6 +16,8 @@ export default function EventGrid({
   displayCount: number
   reload: () => void
 }) {
+  const router = useRouter()
+
   return (
     <ul className="space-y-5 lg:grid lg:grid-cols-2 lg:gap-5 lg:space-y-0 xl:grid-cols-3">
       {isLoadingEvents &&
@@ -25,9 +28,12 @@ export default function EventGrid({
         events.map((event: EventExtended, i: number) => <EventGridItem key={i} event={event} reload={reload} />)}
 
       {!isLoadingEvents && events.length < displayCount && (
-        <li className="flex h-48 cursor-pointer place-items-center justify-center text-gray-300 outline-dashed outline-4 -outline-offset-4 outline-gray-300 transition-all duration-150 hover:scale-105 hover:text-gray-400 hover:outline-gray-400">
+        <button
+          onClick={() => router.push('/createEvent')}
+          className="flex h-48 cursor-pointer place-items-center justify-center text-gray-300 outline-dashed outline-4 -outline-offset-4 outline-gray-300 transition-all duration-150 hover:scale-105 hover:text-gray-400 hover:outline-gray-400"
+        >
           <PlusCircleIcon className="h-12 w-12" />
-        </li>
+        </button>
       )}
     </ul>
   )
